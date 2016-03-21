cc5a0f818af02788b177c413267969682520548f
jiangtaidi <jiangtd@jiangtddemacbook-air.local>
2016-03-21 19:58:09 +0800

    Initial Commit

---
+++ HLDoubleSlideView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLDoubleSlideView
+//
+//  Created by jiangtd on 16/3/21.
+//  Copyright © 2016年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLDoubleSlideView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLDoubleSlideView
+//
+//  Created by jiangtd on 16/3/21.
+//  Copyright © 2016年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
